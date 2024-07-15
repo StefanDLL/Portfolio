b0735981bb9c990a41700a90d5f56ea036cbd3e8
Stefan Le <le_duclong@hotmail.com>
2024-07-15 18:57:31 +0200

    change theme-context component to always show dark theme

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -18,39 +18,39 @@ const ThemeContext = createContext<ThemeContextType | null>(null);
 export default function ThemeContextProvider({
   children,
 }: ThemeContextProviderProps) {
-  const [theme, setTheme] = useState<Theme>("dark");
+  const [theme, setTheme] = useState<Theme>("dark"); // Initial state is "dark"
 
   const toggleTheme = () => {
     if (theme === "light") {
       setTheme("dark");
-      window.localStorage.setItem("theme", "dark");
       document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
     } else {
       setTheme("light");
-      window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.add("light");
       document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme") as Theme | null;
-
-    if (localTheme) {
-      setTheme(localTheme);
-
-      if (localTheme === "dark") {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setTheme("dark");
-      document.documentElement.classList.add("dark");
-    } else {
-      setTheme("light");
-      document.documentElement.classList.remove("dark");
-    }
+    setTheme("dark");
+    document.documentElement.classList.add("dark");
+    document.documentElement.classList.remove("light");
   }, []);
+
+  // const [theme, setTheme] = useState<Theme>("light");
+
+  // const toggleTheme = () => {
+  //   if (theme === "light") {
+  //     setTheme("dark");
+  //     window.localStorage.setItem("theme", "dark");
+  //     document.documentElement.classList.add("dark");
+  //   } else {
+  //     setTheme("light");
+  //     window.localStorage.setItem("theme", "light");
+  //     document.documentElement.classList.remove("dark");
+  //   }
+  // };
 
   // useEffect(() => {
   //   const localTheme = window.localStorage.getItem("theme") as Theme | null;

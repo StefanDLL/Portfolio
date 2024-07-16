6a56f3761fc83fb518b2984a44115b7552cb4174
Stefan Le <le_duclong@hotmail.com>
2024-07-16 21:55:43 +0200

    Added new CV + english pdf cv button

--- components/intro.tsx
+++ components/intro.tsx
@@ -97,10 +97,19 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/CV..pdf"
+          href="/CV-se.pdf"
           download
         >
-          Ladda ner CV{" "}
+          CV (Se){" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition " />
+        </a>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/CV-en.pdf"
+          download
+        >
+          CV (En){" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition " />
         </a>
 
